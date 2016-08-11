4f83c73ed9cf8a45ab970e69941a01b4236304ef
Keith Chester <kchester@gmail.com>
2016-08-11 14:49:19 -0500

    feature: finished story

--- story.js
+++ story.js
@@ -4,4 +4,10 @@ module.exports =
 	"start": require('./Story/start.json'),
 	"east": require('./Story/east.json'),
 	"fishing": require('./Story/fishing.json'),
+	"north": require('./Story/north.json'),
+	"boat": require('./Story/boat.json'),
+	"house": require('./Story/house.json'),
+	"oldlady": require('./Story/oldlady.json'),
+	"south": require('./Story/south.json'),
+	"west": require('./Story/west.json')
 };
